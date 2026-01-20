cdec69fdb568ebbfdf9ef67f45b3bf43a4ec9369
KaeTheDev <kaethedev@gmail.com>
2026-01-20 16:52:48 -0500

    Hooked userRoutes to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Import libraries
 const express = require('express');
 const dotenv = require('dotenv');
+const userRoutes = require("./routes/userRoutes");
 
 // Import DB connection
 const connectDB = require('./config/db');
@@ -17,6 +18,9 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Server is running! 🚀');
 });
+
+// Use the user routes
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
