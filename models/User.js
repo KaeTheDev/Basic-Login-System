3f52d2ae1f55a9b7a07546cb1be103b54113d803
KaeTheDev <kaethedev@gmail.com>
2026-01-20 16:48:30 -0500

    Added pre-save hook to User model

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const { default: bcrypt } = require("bcryptjs");
 const mongoose = require("mongoose");
 
 // Destructure Schema from mongoose
@@ -23,4 +24,13 @@ const userSchema = new Schema({
     },
 });
 
-module.exports = mongoose.model("User", useSchema);
+// Pre-save hook to hash password
+userSchema.pre("save", async function(next){
+    if(this.isNew || this.isModified("password")) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+    next();
+});
+
+module.exports = mongoose.model("User", userSchema);
